bbedee407b7845762d6730f09da3c39a6dd5b2ee
papazock <papazock@windowslive.com>
2014-09-24 00:06:05 +0300

    Map Again :)

--- app/src/main/java/cinemetroproject/cinemetro/MapActivity.java
+++ app/src/main/java/cinemetroproject/cinemetro/MapActivity.java
@@ -1,16 +1,18 @@
 package cinemetroproject.cinemetro;
 
+import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
 
-public class MapActivity extends ActionBarActivity {
+public class MapActivity extends Activity {
 
 
     private GoogleMap mΜap;
@@ -18,6 +20,7 @@ public class MapActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_map);
         mΜap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
